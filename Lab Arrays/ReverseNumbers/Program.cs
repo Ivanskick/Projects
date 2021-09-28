c7516a22f2920671867de398cce1b0d482fbc03c
Ivanskick <i.shkodrova2017@pmg-pernik.com>
2021-09-28 11:34:12 +0300

    Add Project

---
+++ Lab Arrays/ReverseNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+            }
+            Console.WriteLine(string.Join(" ", nums.Reverse()));
+        }
+    }
+}
