a2c19fbae1eaad2b8fec761c65a1321f3924115f
Ivanskick <i.shkodrova2017@pmg-pernik.com>
2021-09-29 09:04:19 +0300

    print even numbers

---
+++ Lab Arrays/PrintEvenNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            var result = new List<int>();
+
+            foreach (var item in nums)
+            {
+                if (item % 2 == 0)
+                {
+                    result.Add(item);
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
