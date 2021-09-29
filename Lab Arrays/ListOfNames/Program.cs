9bc10967f8470e6f7c55c53565b241504996f9fa
Ivanskick <i.shkodrova2017@pmg-pernik.com>
2021-09-29 08:58:33 +0300

    list

---
+++ Lab Arrays/ListOfNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split(", ").ToList();
+
+            foreach (var item in list)
+            {
+                var person = item.Split().ToList();
+                person.Reverse();
+                Console.WriteLine(string.Join(" ", person));
+            }
+        }
+    }
+}
