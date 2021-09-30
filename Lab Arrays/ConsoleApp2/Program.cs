363bf6cc2674eb65ea979ac6b70d326c11524ce2
Ivanskick <i.shkodrova2017@pmg-pernik.com>
2021-09-30 13:05:22 +0300

    name eddit

--- Lab Arrays/ConsoleApp2/Program.cs
+++ Lab Arrays/ConsoleApp2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace ConsoleApp2
+namespace SumEvenNumbers
 {
     class Program
     {
--- Lab Arrays/ListManipulationBasics/Program.cs
+++ Lab Arrays/ListManipulationBasics/Program.cs
@@ -1,16 +1,46 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
-
-namespace ListManipulationBasics
+namespace List
 {
-    class Program
+    class ListManipulationBasics
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            List<int> list = new List<int>();
-            {
 
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                string[] commandArray = command.Split();
+
+                if (commandArray[0] == "Add")
+                {
+                    int numberToAdd = int.Parse(commandArray[1]);
+                    numbers.Add(numberToAdd);
+                }
+                else if (commandArray[0] == "Remove")
+                {
+                    int removeNumber = int.Parse(commandArray[1]);
+                    numbers.Remove(removeNumber);
+                }
+                else if (commandArray[0] == "RemoveAt")
+                {
+                    int removeAtIndex = int.Parse(commandArray[1]);
+                    numbers.RemoveAt(removeAtIndex);
+                }
+
+                else if (commandArray[0] == "Insert")
+                {
+                    int insertIndex = int.Parse(commandArray[1]);
+                    int insertNumber = int.Parse(commandArray[2]);
+                    numbers.Insert(insertIndex, insertNumber);
+                }
+
+                command = Console.ReadLine();
             }
+            Console.WriteLine(string.Join(" ", numbers));
         }
     }
 }
