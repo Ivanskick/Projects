dfc4df453eb37f3e111187b9bfb5ea0bf13a37b0
Ivanskick <i.shkodrova2017@pmg-pernik.com>
2021-09-30 12:56:51 +0300

    ListManipulationAdvanced

---
+++ Lab Arrays/ListManipulationBasics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListManipulationBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            {
+
+            }
+        }
+    }
+}
